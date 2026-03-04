e66ee9f76f54da455859038016f71e03f18e7ada
Jawdat <jzs.maxstudio@gmail.com>
2026-03-04 17:26:53 +0300

    kjhj0

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,6 +7,7 @@ import nextIntlPlugin from "next-intl/plugin";
 const withNextIntl = nextIntlPlugin("./modules/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+	serverExternalPackages: ["@prisma/client", "@prisma/adapter-pg"],
 	experimental: {
 		optimizePackageImports: [
 			"lucide-react",
